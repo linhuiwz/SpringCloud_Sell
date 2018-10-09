d3afd18d3ca88f23da384f343f7114bbf0395f99
unknown <003419@xhh.com>
2018-10-09 15:57:47 +0800

    'api-gateway'

---
+++ api-gateway/src/main/java/com/imooc/apigateway/filter/AuthFilter.java
@@ -0,0 +1,47 @@
+package com.imooc.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 权限拦截（区分买家和卖家）
+ */
+@Component
+public class AuthFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        /**
+         * /order/create 创建订单，只能买家访问，cookie里有openid
+         * /order/finish 完结订单，只能卖家访问，cookie里有token，并且对应的redis里有值
+         * /product/list 都可访问
+         */
+
+        return null;
+    }
+}
